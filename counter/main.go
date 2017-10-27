a1bdccc85d7b979266f633982d863a8b645a94ef
Kevin Haines <kevin.haines@appsbroker.com>
2017-10-27 11:18:47 +0100

    counter example

---
+++ counter/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type IdGenerator func() int
+
+func NewIdGen() IdGenerator {
+	ch := make(chan (int))
+	server := func() {
+		var counter int
+		for {
+			ch <- counter
+			counter++
+		}
+	}
+	go server()
+
+	return func() int {
+		return <-ch
+	}
+}
+
+func main() {
+
+	gen := NewIdGen()
+
+	for i := 0; i < 100000; i++ {
+		me := i
+		go func() {
+			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+			fmt.Printf("routine %d got %d\n", me, gen())
+		}()
+	}
+	time.Sleep(time.Second * 2)
+
+}
